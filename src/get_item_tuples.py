dba0a1e19985b005ffd352e3883b9ac123bbf266
Andres Rodriguez <anrocox@gmail.com>
2014-07-06 17:00:14 -0500

    How to get a item of the tuple in python?

---
+++ src/get_item_tuples.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 3, 2014
+
+@author: anroco
+
+How to get a item of the tuple in python?
+
+En python ¿como obtener un item de una tupla?
+'''
+
+#create a tuple
+tupla = ('a', 1, 'b', 2, 'c', 3)
+print (tupla)
+
+#get item of the tuple by index
+item = tupla[2]
+print (item)
+
+#get item by index negative
+item = tupla[-3]
+print (item)
